b9c0e1588a4e5d4737c804a9389a1b080339a216
S1im_Sh4dy <mihakurd@gmail.com>
2023-02-07 00:18:17 +0300

    - split the function into parts - Corrected Plural in titles - Renamed some functions and variables - Optimized JSON decoding - Now I check the HTTP status of the response - Added error handling VK api - Formatted the code

--- main.py
+++ main.py
@@ -4,60 +4,78 @@ import sys
 import requests
 from dotenv import load_dotenv
 from urllib.parse import urljoin
+import traceback
 
 
-def get_comics(url: str, filename):
-    response_comics = requests.get(url=urljoin(url, f'{filename}/info.0.json'))
-    response_comics.raise_for_status()
+class VkApiError(requests.HTTPError):
+    """HTTP Error from vk response"""
+    pass
 
-    response_img = requests.get(url=response_comics.json()['img'])
-    response_img.raise_for_status()
 
-    comment = response_comics.json()['alt']
+def raise_vk_response(response):
+    error_response = response.json()
+    if 'error' in error_response:
+        raise VkApiError(error_response['error']['error_msg'])
+
+
+def get_comic(url: str, filename):
+    comic_response = requests.get(url=urljoin(url, f'{filename}/info.0.json'))
+    comic_response.raise_for_status()
+
+    image_response = requests.get(url=comic_response.json()['img'])
+    image_response.raise_for_status()
+
+    comment = comic_response.json()['alt']
 
     with open(f'{filename}.jpg', 'wb') as file:
-        file.write(response_img.content)
+        file.write(image_response.content)
 
     return comment
 
 
-def get_upload_url(token, group_id, filename):
+def download_comic(token, group_id, filename):
     headers = {'Authorization': f'Bearer {token}'}
-    response_upload_server = requests.get(
+    upload_server_response = requests.get(
         url='https://api.vk.com/method/photos.getWallUploadServer',
         params={'v': 5.131, 'group_id': group_id},
-        headers=headers
+        headers=headers,
     )
+    upload_server_response.raise_for_status()
+    raise_vk_response(upload_server_response)
 
     with open(f'{filename}.jpg', 'rb') as file:
-        upload_url = response_upload_server.json()['response']['upload_url']
-        response_download = requests.post(url=upload_url, files={'photo': file})
+        upload_url = upload_server_response.json()['response']['upload_url']
+        upload_response = requests.post(url=upload_url, files={'photo': file})
 
-    response_download.raise_for_status()
+    upload_response.raise_for_status()
+    raise_vk_response(upload_response)
 
-    os.remove(f'{filename}.jpg')
-
-    return response_download
+    return upload_response
 
 
-def save_photo(group_id, headers, response):
-    response_saver = requests.post(
+def save_photo(group_id, token, response):
+    response = response.json()
+    headers = {'Authorization': f'Bearer {token}'}
+    saving_comic_response = requests.post(
         url='https://api.vk.com/method/photos.saveWallPhoto',
         params={'v': 5.131,
                 'group_id': group_id,
-                'photo': response.json()['photo'],
-                'server': response.json()['server'],
-                'hash': response.json()['hash']
+                'photo': response['photo'],
+                'server': response['server'],
+                'hash': response['hash']
                 },
-        headers=headers
+        headers=headers,
     )
-    response_saver.raise_for_status()
-    return response_saver
+    saving_comic_response.raise_for_status()
+    raise_vk_response(saving_comic_response)
+    return saving_comic_response
 
 
-def posting_photo(group_id, headers, comment, response):
-    attachments = f'photo{response.json()["response"][0]["owner_id"]}_{response.json()["response"][0]["id"]}'
-    response_posting = requests.post(
+def post_photo(group_id, token, comment, response):
+    response = response.json()
+    headers = {'Authorization': f'Bearer {token}'}
+    attachments = f'photo{response["response"][0]["owner_id"]}_{response["response"][0]["id"]}'
+    posting_comic_response = requests.post(
         url='https://api.vk.com/method/wall.post',
         params={
             'v': 5.131, 'owner_id': f'-{group_id}',
@@ -67,26 +85,31 @@ def posting_photo(group_id, headers, comment, response):
         },
         headers=headers,
     )
-    response_posting.raise_for_status()
+    posting_comic_response.raise_for_status()
+    raise_vk_response(posting_comic_response)
 
 
 def main():
     load_dotenv()
-    headers = {'Authorization': f"Bearer {os.environ['VK_TOKEN']}"}
-    vk_token, vk_group_id = os.environ['VK_TOKEN'], os.environ['VK_GROUP_ID']
-    comics_num = random.randint(0, 2733)
+    vk_token, vk_group_id, last_comic_num = os.environ['VK_TOKEN'], os.environ['VK_GROUP_ID'], 2733
+    comic_num = random.randint(0, last_comic_num)
 
     try:
-        comment = get_comics(url='https://xkcd.com/', filename=comics_num)
-        response_download = get_upload_url(vk_token, vk_group_id, filename=comics_num)
-        response_saver = save_photo(vk_group_id, headers, response_download)
-        posting_photo(vk_group_id, headers, comment,  response_saver)
+        comment = get_comic(url='https://xkcd.com/', filename=comic_num)
+        upload_response = download_comic(vk_token, vk_group_id, filename=comic_num)
+        saving_comic_response = save_photo(vk_group_id, vk_token, upload_response)
+        post_photo(vk_group_id, vk_token, comment, saving_comic_response)
 
+    except VkApiError:
+        print(traceback.format_exc(), file=sys.stderr)
     except requests.exceptions.HTTPError:
-        print(f'HTTP Error in {comics_num} comics', file=sys.stderr)
+        print(traceback.format_exc(), file=sys.stderr)
     except requests.exceptions.ConnectionError:
-        print('Connection Error')
+        print(traceback.format_exc(), file=sys.stderr)
+    finally:
+        os.remove(f'{comic_num}.jpg')
 
 
 if __name__ == '__main__':
     main()
+
